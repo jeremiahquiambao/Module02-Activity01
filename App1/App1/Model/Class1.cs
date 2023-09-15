c22edd459d8d8fdf76c73f753b1150b6d59f3f11
User <User@B20>
2023-09-15 11:44:12 +0800

    Add project files.

---
+++ App1/App1/Model/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Model
+{
+    class Class1
+    {
+        public string Subjectcode { get; set; }
+
+        public string SubjectTitle { get; set; }
+
+        public int Unit {  get; set; }
+    }
+}
---
+++ App1/App1/ViewModel/TaskViewModel.cs
@@ -0,0 +1,23 @@
+﻿using App1.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.ViewModel
+{
+    class TaskViewModel
+    {
+        public Class1 TaskModelSet { get; set; }
+
+        public TaskViewModel()
+        {
+            TaskModelSet = new Class1()
+            {
+                Subjectcode = "PDC06",
+                SubjectTitle = "Xamarin Mobile Programming",
+                Unit = 3,
+
+            };
+        }
+    }
+}
